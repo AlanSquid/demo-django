1db868b8a82bf450f595445d22106473adefc00f
Alanyu <squid78313@gmail.com>
2024-11-04 09:33:50 +0800

    add TodoItemViewSet的action: get, post

--- src/todo/urls.py
+++ src/todo/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 from . import views
+from .views import TodoItemViewSet
+
+# 定義ViewSet的action
+todo_list = TodoItemViewSet.as_view({
+    'get': 'list',
+    'post': 'create'
+})
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('todos/', views.todo_list, name='todo_list')
+    path('todos/', todo_list, name='todo_list')
 ]
